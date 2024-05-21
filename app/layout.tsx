8b3f86a3ea806240ed4057bfea457cee143ca814
Ripic Zhang <56013580+ylwind@users.noreply.github.com>
2024-05-21 19:31:41 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from 'sonner';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'My IP and Network Information - myip.moe',
+  title: 'My IP and Network Information - ip.eipic.site',
   description: 'My IP and Network Information',
   icons: {
     icon: '/favicon.png',
@@ -47,16 +47,10 @@ export default function RootLayout({
             {children}
             <footer className="mb-4 text-xs text-slate-600">
               <span className="mr-0.5">©</span>
-              <a href="https://myip.moe" className="mr-1 hover:underline">
-                MyIP.moe.
+              <a href="https://ip.ripic.site" className="mr-1 hover:underline">
+                ip.ripic.site
               </a>
               <span>All Rights Reserved.</span>
-              <a
-                className="pl-1 hover:underline"
-                href="https://github.com/copilot-is/myip"
-              >
-                GitHub
-              </a>
             </footer>
           </main>
         </div>
